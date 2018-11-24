be376cea567a2b2deb16cd51c1a625aa6db597bf
Eito Katagiri <eitoball@gmail.com>
2018-11-24 15:00:55 +0900

    add frozen literal string magic comment for Style/FrozenStringLiteralComment

--- spec/models/ingest_measurment_spec.rb
+++ spec/models/ingest_measurment_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 RSpec.describe IngestMeasurement, type: :model do
@@ -6,6 +8,10 @@ RSpec.describe IngestMeasurement, type: :model do
   describe '#data_for_device' do
     before do
       put_template_for_ingest_measurements(described_class.__elasticsearch__.client)
+      [
+        { device_urn: 'safecast:374304606', device: '374304606' },
+        { device_urn: 'safecast:374304606', device: '374304606' }
+      ]
       add_index_to_ingest_measurements(described_class.__elasticsearch__.client, device_urn: 'safecast:374304606', device: '374304606')
     end
 
